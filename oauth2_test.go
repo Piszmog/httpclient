36b0550ba37e03062acb92571ce65410d4e4c099
Piszmog <piszmogcloud@gmail.com>
2018-09-21 16:30:27 -0600

    fix unit test

--- oauth2_test.go
+++ oauth2_test.go
@@ -1,17 +1,11 @@
 package httpclient
 
 import (
-	"github.com/Piszmog/cfservices"
 	"testing"
 )
 
 func TestCreateOAuth2Client(t *testing.T) {
-	credentials := &cfservices.Credentials{
-		AccessTokenUri: "tokenUri",
-		ClientSecret:   "clientSecret",
-		ClientId:       "clientId",
-	}
-	client, err := CreateOAuth2Client(credentials)
+	client, err := CreateOAuth2Client("clientId", "clientSecret", "accessTokenURI")
 	if err != nil {
 		t.Errorf("failed to create oauth2 client with error %v", err)
 	}
@@ -20,37 +14,12 @@ func TestCreateOAuth2Client(t *testing.T) {
 	}
 }
 
-func TestCreateOAuth2ClientWhenCredentialsAreNil(t *testing.T) {
-	client, err := CreateOAuth2Client(nil)
-	if err == nil {
-		t.Error("expected an error when no credentials are passed")
-	}
-	if client != nil {
-		t.Error("able to create an oauth2 client with nil credentials")
-	}
-}
-
 func TestCreateOauth2Config(t *testing.T) {
-	credentials := &cfservices.Credentials{
-		AccessTokenUri: "tokenUri",
-		ClientSecret:   "clientSecret",
-		ClientId:       "clientId",
-	}
-	config, err := CreateOAuth2Config(credentials)
+	config, err := CreateOAuth2Config("clientId", "clientSecret", "accessTokenURI")
 	if err != nil {
 		t.Errorf("failed to create oauth2 with errpr %v", err)
 	}
 	if config == nil {
 		t.Error("failed to create oauth2 config")
-	}
-}
-
-func TestCreateOauth2ConfigWhenCredentialsNil(t *testing.T) {
-	config, err := CreateOAuth2Config(nil)
-	if err == nil {
-		t.Error("expected an error when passing nil credentials when creating oauth2 config")
-	}
-	if config != nil {
-		t.Error("is able to create oauth2 config when credentials are nil")
 	}
 }
